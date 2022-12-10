28df8a536ca05adffb48f94572407d0d740726cb
lucascyrne <cyrne24@gmail.com>
2022-12-10 18:54:36 -0300

    dev: document updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet" />
+                <meta name="description" content="MERN stack developer looking for a job" />
+                <meta name="keywords" content="Full Stack Web Developer, Freelancer, MERN developer" />
             </Head>
             <body className='bg-fixed bg-gradient-to-r from-green to-blue-400 dark:from-dark-500 dark:to-dark-700 dark:text-white'>
                 <Main />
