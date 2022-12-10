48868a1c22de4dd0a76f1d4fe9fe4913dd8a678e
lucascyrne <cyrne24@gmail.com>
2022-12-10 18:49:45 -0300

    dev: new packages, configs and minor updates

--- type.ts
+++ type.ts
@@ -11,3 +11,16 @@ export interface ISkill {
     level: string
     Icon: IconType
 }
+
+export interface IProject {
+    id: number
+    name: string
+    description: string
+    image_path: string
+    deployed_url: string
+    github_url: string
+    category: Category[]
+    key_techs: string[]
+}
+
+export type Category = 'react' | 'node' | 'express' | 'django' | 'mongo'
